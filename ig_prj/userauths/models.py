4f14cd16c00aacd5ffb1b2cabce2bb286ac3ebf7
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-28 12:15:15 +0900

    update: image resize

--- ig_prj/userauths/models.py
+++ ig_prj/userauths/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from PIL import Image
+from django.db.models.signals import post_save
+
 from post.models import Post
 
 class Profile(models.Model):
@@ -13,5 +16,18 @@ class Profile(models.Model):
     url = models.URLField(max_length=200, null=True, blank=True)
     favourite = models.ManyToManyField(Post, blank=True)
 
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f'{self.user.username} - Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
